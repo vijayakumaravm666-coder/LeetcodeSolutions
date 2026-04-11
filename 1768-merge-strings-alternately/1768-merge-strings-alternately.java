51605d9d43dd774881341c7ded8c0ede5715e219
vijayakumaravm666-coder <vijayakumaravm666@gmail.com>
2026-04-11 22:49:54 +0530

    Time: 1 ms (95.64%), Space: 43.1 MB (68.84%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        StringBuilder s=new StringBuilder();
+        for(int i=0;i<n || i<m;i++){
+            if(i<n){
+                s.append(word1.charAt(i));
+            }
+            if(i<m){
+                s.append(word2.charAt(i));
+            }
+        }
+        return s.toString();
+    }
+}
